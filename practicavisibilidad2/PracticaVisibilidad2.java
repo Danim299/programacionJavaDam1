5a9f8f3f7f9399977cbf59da4bb493c22191daec
Daniel Medina <133040190+Danim299@users.noreply.github.com>
2023-05-15 16:33:14 +0200

    Add files via upload

---
+++ practicavisibilidad2/PracticaVisibilidad2.java
@@ -0,0 +1,11 @@
+package practicavisibilidad2;
+
+import practicavisibilidad.Persona;
+
+public class PracticaVisibilidad2 {
+
+    public static void main(String[] args) {
+        Persona p = new Persona("Daniel","Medina");
+        p.saludar();
+    }
+}
